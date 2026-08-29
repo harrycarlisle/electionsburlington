a38233e29dacbd971faec4c0fa9eedaa5779cbb5
ina <63021848+harrycarlisle@users.noreply.github.com>
2026-08-29 12:58:48 -0400

    Refresh Explore recurrence assets

--- explore-recurrence.js
+++ explore-recurrence.js
@@ -1,4 +1,4 @@
-import { mergeExploreEvents, expandRecurringEvent, FARMERS_MARKET_RECURRENCE } from '/lib/explore-recurrence.mjs';
+import { mergeExploreEvents, expandRecurringEvent, FARMERS_MARKET_RECURRENCE } from '/lib/explore-recurrence.mjs?v=20260829images1';
 
 window.BurlingtonExploreRecurrence = {
   mergeExploreEvents,
